f6f5124bfb708b8fc3cf437a167a3508ffabd581
OshanJayawardana <oshanjayawardanav100@gmail.com>
2025-01-07 07:02:12 +0000

    class images added

--- plot_membership.py
+++ plot_membership.py
@@ -27,4 +27,14 @@ for i, ax in enumerate(axs.flat):
 plt.suptitle("Members")
 plt.savefig("figures/members.png")
 
+# from each class(0-43) plot 5 random images
+label_points = {i: [img.permute(1, 2, 0).numpy() for j, img in enumerate(dataset.imgs) if dataset.labels[j] == i] for i in range(44)}
+fig, axs = plt.subplots(44, 4, figsize=(12.8, 140.8))
+for i in range(44):
+    random.shuffle(label_points[i])
+    for j, ax in enumerate(axs[i]):
+        ax.imshow(label_points[i][j])
+        ax.axis("off")
+plt.savefig("figures/class_images.png")
+
 
